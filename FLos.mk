117a757eda043b3c7bd3637aa523bd90baaf62a9
TheAtt1la <alittauwu@gmail.com>
2022-12-08 02:05:09 -0300

    first commit

---
+++ FLos.mk
@@ -0,0 +1,15 @@
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/BoardConfigSoong.mk)
+$(call inherit-product, device/lineage/sepolicy/common/sepolicy.mk)
+-include vendor/lineage/build/core/config.mk
+
+TARGET_NO_KERNEL_OVERRIDE := true                                       
+TARGET_NO_KERNEL_IMAGE := true
+TARGET_USES_PREBUILT_VENDOR_SEPOLICY := true                           
+TARGET_HAS_FUSEBLK_SEPOLICY_ON_VENDOR := true
+
+SELINUX_IGNORE_NEVERALLOWS := true
+TARGET_BOOT_ANIMATION_RES := 1080
+
+#FLOS_TYPE := vanilla
+
---
+++ README.md
@@ -0,0 +1,1 @@
+# ForkLos-patches
